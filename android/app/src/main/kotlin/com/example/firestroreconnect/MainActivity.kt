6d4bd33447a61dbc658f53ef1b8d1c667dd38c0b
vatiza <rakibur.mac@gmail.com>
2023-11-06 23:18:26 +0600

    design part start

---
+++ android/app/src/main/kotlin/com/example/firestroreconnect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firestroreconnect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/loginpage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Demo'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    labelText: 'Email'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      labelText: 'Phone'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
